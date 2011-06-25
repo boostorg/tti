53a9392bba678c92c562cbd92a999709cc36e2f5
Edward Diener <eldiener@tropicsoft.com>
2011-06-25 13:54:58 +0000

    Added copyright info and boost license specification. Corrected doc error. Remove HTML in order to regenerate it.
    [SVN r72751]

--- test/test_has_static_mem_data_fail2.cpp
+++ test/test_has_static_mem_data_fail2.cpp
@@ -1,3 +1,9 @@
+
+//  (C) Copyright Edward Diener 2011
+//  Use, modification and distribution are subject to the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt).
+
 #include "test_has_static_mem_data.hpp"
 #include <boost/mpl/assert.hpp>
 
